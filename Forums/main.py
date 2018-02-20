d4332a1c9993625215480531551a1c15c728a4ed
Malek salem <malek.salem.14@gmail.com>
2018-02-20 17:23:31 +0100

    some class update

---
+++ Forums/main.py
@@ -0,0 +1,9 @@
+import models
+
+membre1 = models.Members("Ahmed Haj",25)
+member2 = models.Members("Malek",19)
+
+post1 = models.Post("Best programming language!","python")
+post2 = models.Post("Hacking","We must secure our computers!")
+print post2
+print post1
---
+++ Forums/models.py
@@ -0,0 +1,11 @@
+class Members():
+	def __init__(self,username,age):
+		self.name = username
+		self.age = age
+
+class Post():
+	def __init__(self,title,content):
+		self.title	= title 
+		self.content = content
+	def __str__(self):
+		return self.title +"\t"+ self.content 
